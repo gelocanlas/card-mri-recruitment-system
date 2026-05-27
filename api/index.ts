40b6c1226917189d963d8dd270da7b34f35a028d
Your Name <you@example.com>
2026-05-27 20:34:27 +0800

    Debug: catch server import error and return it

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,12 @@
-import app from "../server";
+let app: any;
+try {
+  app = require("../server").default || require("../server");
+} catch (e: any) {
+  const errorMsg = `ImportError: ${e.message}`;
+  app = (req: any, res: any) => {
+    res.writeHead(500, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ error: errorMsg, stack: e.stack?.split("\n").slice(0, 5).join("\\n") }));
+  };
+}
+
 export default app;
