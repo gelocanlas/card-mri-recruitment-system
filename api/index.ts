3017146706f966584db05f40dc88920d28ab3fa4
Your Name <you@example.com>
2026-05-27 20:36:44 +0800

    Revert to ESM import pattern for api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,2 @@
-let app: any;
-try {
-  app = require("../server").default || require("../server");
-} catch (e: any) {
-  const errorMsg = `ImportError: ${e.message}`;
-  app = (req: any, res: any) => {
-    res.writeHead(500, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ error: errorMsg, stack: e.stack?.split("\n").slice(0, 5).join("\\n") }));
-  };
-}
-
+import app from "../server";
 export default app;
